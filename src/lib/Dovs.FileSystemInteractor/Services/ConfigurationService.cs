4d004f43e08754d322d0799636f0aff31b25225f
Rehab Ragab <rehab.abotalep@gmail.com>
2024-11-13 12:10:54 +0000

    Improvement - Configuration - Handle Multi Configurations

--- src/lib/Dovs.FileSystemInteractor/Services/ConfigurationService.cs
+++ src/lib/Dovs.FileSystemInteractor/Services/ConfigurationService.cs
@@ -13,9 +13,10 @@ namespace Dovs.FileSystemInteractor.Services
 
         public ConfigurationService()
         {
+            var environment = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT") ?? "Production";
             var builder = new ConfigurationBuilder()
                 .SetBasePath(AppContext.BaseDirectory)
-                .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true);
+                .AddJsonFile($"appsettings.{environment}.json", optional: true, reloadOnChange: true);
 
             _configuration = builder.Build();
         }
